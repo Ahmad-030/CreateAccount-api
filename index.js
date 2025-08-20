99e5c0f95c20c825589e8d4dfa8dca20ac0c0d36
shadow0300 <ahmadasif20222@gmail.com>
2025-08-21 01:46:31 +0500

    mongodb

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import mongoose from "mongoose";
+import Users from "./models/Users.js";
 
 const app = express();
 app.use(express.json());
@@ -7,35 +9,89 @@ app.listen(5000, () => {
   console.log("Server is Online now 🚀");
 });
 
+async function dbConnect () {
+  try {
+    await mongoose.connect('mongodb+srv://shadow:shadow11@cluster0.z3p61vh.mongodb.net/')
+    console.log('database connected')
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+dbConnect()
+
 // Root Route
 app.get("/", (req, res) => {
   res.send({ message: " API Working 🚀" });
 });
 
-// Create Account API
-app.post("/create-account", (req, res) => {
-  const { username, email, password, number } = req.body; 
-  let errors = [];
 
-  if (!username || !email || !password || !number) {
-    return res.status(400).json({ message: "All fields (username, email, password, number) are required" });
+app.get('/users', async (req, res) => {
+  try {
+    const data = await Users.find();
+
+    res.json({message: "users fetched", data: data})
+  } catch (e) {
+    console.log(e)
+    res.status(500).json({message: "something went wrong"})
   }
+})
 
-  if (!email.includes("@") || !email.includes("gmail.com")) {
-    errors.push("Invalid email format");
+
+app.post('/users', async (req, res) => {
+  try {
+    const {username, email, password, number} = req.body;
+
+    const isEmailExists = await Users.findOne({email: email});
+
+    if(isEmailExists){
+      return res.status(500).json({
+        message: "email already exists"
+      })
+    }
+
+    const usernameExists = await Users.findOne({username: username});
+
+    if(usernameExists){
+      return res.status(500).json({
+        message: "username already exists"
+      })
+    }
+
+
+    await Users.create({username, email, password,number})
+
+    res.json({message: "data saved"})
+  } catch (e) {
+    console.log(e)
+    res.status(500).json({message: "something went wrong"})
   }
+})
 
-  if (password.length < 6) {
-    errors.push("Password must be at least 6 characters long");
-  }
+// // Create Account API
+// app.post("/create-account", (req, res) => {
+//   const { username, email, password, number } = req.body; 
+//   let errors = [];
 
-  if (!/^\d+$/.test(number)) {
-    errors.push("Number must contain only digits");
-  }
+//   if (!username || !email || !password || !number) {
+//     return res.status(400).json({ message: "All fields (username, email, password, number) are required" });
+//   }
 
-  if (errors.length > 0) {
-    return res.status(400).json({ errors });
-  }
+//   if (!email.includes("@") || !email.includes("gmail.com")) {
+//     errors.push("Invalid email format");
+//   }
 
-  res.json({ message: "Account created successfully!" });
-});
+//   if (password.length < 6) {
+//     errors.push("Password must be at least 6 characters long");
+//   }
+
+//   if (!/^\d+$/.test(number)) {
+//     errors.push("Number must contain only digits");
+//   }
+
+//   if (errors.length > 0) {
+//     return res.status(400).json({ errors });
+//   }
+
+//   res.json({ message: "Account created successfully!" });
+// });
